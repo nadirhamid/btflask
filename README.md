edc525d532be53427c5e074859ad1da925fd735f
Austin Vern Songer <austinvernsonger@outlook.com>
2016-09-26 08:42:29 -0500

    First
    Signed-off-by: Austin Vern Songer <austinvernsonger@outlook.com>

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# btflask
+# Braintree Flask Example
+[![Build Status](https://travis-ci.org/braintree/braintree_flask_example.svg?branch=master)](https://travis-ci.org/braintree/braintree_flask_example)
+
+An example Braintree integration for python in the Flask framework.
+
+## Setup Instructions
+
+1. Install requirements:
+  ```sh
+  pip install -r requirements.txt
+  ```
+
+2. Copy the `example.env` file to `.env` and fill in your Braintree API credentials. Credentials can be found by navigating to Account > My User > View Authorizations in the Braintree Control Panel. Full instructions can be [found on our support site](https://articles.braintreepayments.com/control-panel/important-gateway-credentials#api-credentials).
+
+3. Start server:
+  ```sh
+  python app.py
+  ```
+
+## Deploying to Heroku
+
+You can deploy this app directly to Heroku to see the app live. Skip the setup instructions above and click the button below. This will walk you through getting this app up and running on Heroku in minutes.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/braintree/braintree_flask_example&env[BT_ENVIRONMENT]=sandbox)
+
+## Running tests
+
+Unit tests do not make API calls to Braintree and do not require Braintree credentials. You can run this project's unit tests by calling `python test_app.py` on the command line.
+
+## Pro Tips
+
+- The `application.cfg.example` contains an `APP_SECRET_KEY` setting. Even in development you should [generate your own custom secret key for your app](http://flask.pocoo.org/docs/0.10/quickstart/#sessions).
+
+## Help
+
+ * Found a bug? Have a suggestion for improvement? Want to tell us we're awesome? [Submit an issue](https://github.com/braintree/braintree_flask_example/issues)
+ * Trouble with your integration? Contact [Braintree Support](https://support.braintreepayments.com/) / support@braintreepayments.com
+ * Want to contribute? [Submit a pull request](https://help.github.com/articles/creating-a-pull-request)
+
+## Disclaimer
+
+This code is provided as is and is only intended to be used for illustration purposes. This code is not production-ready and is not meant to be used in a production environment. This repository is to be used as a tool to help merchants learn how to integrate with Braintree. Any use of this repository or any of its code in a production environment is highly discouraged.
---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import braintree
+
+app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+
+braintree.Configuration.configure(
+    os.environ.get('BT_ENVIRONMENT'),
+    os.environ.get('BT_MERCHANT_ID'),
+    os.environ.get('BT_PUBLIC_KEY'),
+    os.environ.get('BT_PRIVATE_KEY')
+)
+
+TRANSACTION_SUCCESS_STATUSES = [
+    braintree.Transaction.Status.Authorized,
+    braintree.Transaction.Status.Authorizing,
+    braintree.Transaction.Status.Settled,
+    braintree.Transaction.Status.SettlementConfirmed,
+    braintree.Transaction.Status.SettlementPending,
+    braintree.Transaction.Status.Settling,
+    braintree.Transaction.Status.SubmittedForSettlement
+]
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('new_checkout'))
+
+@app.route('/checkouts/new', methods=['GET'])
+def new_checkout():
+    client_token = braintree.ClientToken.generate()
+    return render_template('checkouts/new.html', client_token=client_token)
+
+@app.route('/checkouts/<transaction_id>', methods=['GET'])
+def show_checkout(transaction_id):
+    transaction = braintree.Transaction.find(transaction_id)
+    result = {}
+    if transaction.status in TRANSACTION_SUCCESS_STATUSES:
+        result = {
+            'header': 'Sweet Success!',
+            'icon': 'success',
+            'message': 'Your test transaction has been successfully processed. See the Braintree API response and try again.'
+        }
+    else:
+        result = {
+            'header': 'Transaction Failed',
+            'icon': 'fail',
+            'message': 'Your test transaction has a status of ' + transaction.status + '. See the Braintree API response and try again.'
+        }
+
+    return render_template('checkouts/show.html', transaction=transaction, result=result)
+
+@app.route('/checkouts', methods=['POST'])
+def create_checkout():
+    result = braintree.Transaction.sale({
+        'amount': request.form['amount'],
+        'payment_method_nonce': request.form['payment_method_nonce'],
+    })
+
+    if result.is_success or result.transaction:
+        return redirect(url_for('show_checkout',transaction_id=result.transaction.id))
+    else:
+        for x in result.errors.deep_errors: flash('Error: %s: %s' % (x.code, x.message))
+        return redirect(url_for('new_checkout'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4567, debug=True)
